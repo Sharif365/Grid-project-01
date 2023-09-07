8dfac32fdde5803a79363a127fbe283f19ec1c3a
Hasanujjaman <h.j.sharif365@gmail.com>
2023-09-07 16:20:57 +0600

    grid css

---
+++ project 01/main.js
@@ -0,0 +1,14 @@
+let long = "Javascript";
+let LONH = "PHP";
+
+
+
+function block(){
+    if(long){
+        let long = LONH;
+        console.log(long + " Ok");
+    }
+    return long
+}
+
+console.log(block());
